eb00676d6519372d6fcdda755b0f2aeeb20303e3
Grigorii <Fastmotion1111@gmail.com>
2020-02-09 17:00:06 +0200

    refactor code

--- src/Components/SitePage/MainPage/index.js
+++ src/Components/SitePage/MainPage/index.js
@@ -1,46 +1,42 @@
 import React, {Component} from 'react'
 import * as THREE from "three/src/Three";
-import Globe from "./animationFon";
+import Globe from "vanta/dist/vanta.globe.min";
 import './style.css'
-class MainPage extends  Component {
 
-    constructor() {
-        super();
-        this.vantaRef = React.createRef()
+class MainPage extends Component {
+
+  constructor() {
+    super();
+    this.vantaRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.vantaEffect = Globe({
+      el: this.vantaRef.current,
+      THREE: THREE
+    })
+  }
+
+  componentWillUnmount() {
+    if (this.vantaEffect) {
+      this.vantaEffect.destroy()
     }
 
-    componentDidMount() {
-        this.vantaEffect = Globe({
-            el: this.vantaRef.current,
-            THREE: THREE
+  }
 
-        })
-    }
-    componentWillUnmount() {
-        if (this.vantaEffect)
-        {this.vantaEffect.destroy()}
+  render() {
+    const {article} = this.props;
 
-    }
-    render() {
+    return (
+      <div className="containerPage" ref={this.vantaRef}>
 
-        const {article} = this.props;
-
-
-        return (
-            <div className="containerPage" ref={this.vantaRef}>
-
-            <div  className="MainPage_text">
-                <h1 className="title">{article[0].title}</h1>
-                <section className="section_text">{article[0].text}</section>
-            </div>
-            </div>
-
-        )
-
-    }
-
+        <div className="MainPage_text">
+          <h1 className="title">{article[0].title}</h1>
+          <section className="section_text">{article[0].text}</section>
+        </div>
+      </div>
+    )
+  }
 }
-
-
 
 export default MainPage
